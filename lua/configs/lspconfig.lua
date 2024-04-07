f79732e89791e5d5bf2e9d0b4c7a2a311e404740
Maksim Grin <maksim.grin@lamoda.ru>
2024-04-07 21:46:50 +0300

    vue support

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,20 +4,48 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" }
+-- local servers = { "html", "cssls" }
+--
+-- -- lsps with default config
+-- for _, lsp in ipairs(servers) do
+--   lspconfig[lsp].setup {
+--     on_attach = on_attach,
+--     on_init = on_init,
+--     capabilities = capabilities,
+--   }
+-- end
 
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
+-- volar
+lspconfig.volar.setup {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+  init_options = {
+    vue = {
+      hybridMode = true,
+    },
+    -- typescript = {
+    --   tsdk = "./node_modules/typescript/lib"
+    -- }
   }
-end
-
+}
 -- typescript
+local ts_ft = { "javascript", "javascriptreact", "typescript", "typescriptreact" }
+local vue_ft = { unpack(ts_ft) }
+table.insert(vue_ft, "vue")
+
 lspconfig.tsserver.setup {
   on_attach = on_attach,
   on_init = on_init,
   capabilities = capabilities,
+  filetypes = vue_ft,
+  init_options = {
+    plugins = {
+      {
+        name = "@vue/typescript-plugin",
+        location = "/home/str1x/.local/share/nvim/mason/packages/vue-language-server/node_modules/@vue/language-server/node_modules/@vue/typescript-plugin",
+        languages = vue_ft,
+      },
+    },
+  },
 }
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,6 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "gd", "<cmd>Telescope lsp_definitions<cr>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
