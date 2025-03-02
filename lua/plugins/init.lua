d05646d419fa462311471626909d82dcae881365
Maksim Grin <maksim.grin@lamoda.ru>
2025-03-03 00:12:00 +0300

    add custom snippets

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -108,11 +108,10 @@ return {
       {
         -- snippet plugin
         "L3MON4D3/LuaSnip",
-        dependencies = "rafamadriz/friendly-snippets",
         opts = { history = true, updateevents = "TextChanged,TextChangedI" },
         config = function(_, opts)
           require("luasnip").config.set_config(opts)
-          require "configs.luasnip"
+          require("configs.luasnip")();
         end,
       },
       -- autopairing of (){}[] etc
